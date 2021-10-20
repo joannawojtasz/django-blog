5cfde90f5f2223f7e6a74bf73110dd7ba7254738
joannawojtasz <joannawojtasz@gmail.com>
2021-10-20 23:10:08 +0000

    add models

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,7 +11,7 @@ import os
 from pathlib import Path
 
 import dj_database_url
-if os.path.isfile("env.py"):
+if os.path.exists("env.py"):
     import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
